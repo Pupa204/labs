a1cfd7263df6e0f0ae6346cc934c915adea6e8fd
Pupa204 <puma.nikolaeva204@gmail.com>
2026-03-12 11:24:57 +0700

    /

--- Lab3/lab.py
+++
@@ -1,9 +0,0 @@
-from interface import InterfaceApp
-from lesson import Lesson
-from fileWorker import FileWorker
-import tkinter as tk
-
-root = tk.Tk()
-repository = FileWorker()
-app = InterfaceApp(root, repository)
-root.mainloop()
